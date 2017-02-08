dd0f39e66814fd70e6ab179fa1c12b39bc6e4b24
lobodol <grobodol@gmail.com>
2017-02-08 13:24:56 +0100

    Initialize modal object

---
+++ app/Resources/public/js/modal.js
@@ -0,0 +1,18 @@
+var Modal = {
+
+    /**
+     * Initialize event listeners & DOM elements.
+     */
+    init: function() {
+
+    },
+
+    /**
+     * Display a message in a modal.
+     *
+     * @param {string} data : the string to display.
+     */
+    open: function(data) {
+        alert(data);
+    }
+};
