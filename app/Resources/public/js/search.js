09edc8c40f06e5b3ac6a6885e7bac44d4cdd570a
lobodol <grobodol@gmail.com>
2017-02-09 11:18:45 +0100

    Add method to SearchEngine to highlight search terms in results

--- app/Resources/public/js/search.js
+++ app/Resources/public/js/search.js
@@ -8,33 +8,54 @@ var SearchEngine = {
     lastSearch: '',
 
     /**
+     * A jQuery object representing the search field.
+     *
+     * @type {object}
+     */
+    searchField: null,
+
+    /**
      * Initialize event listeners.
      */
     init: function () {
-        $('#searchField').keyup(function (ev) {
+        SearchEngine.searchField = $('#searchField');
+
+        // When typing something in search field.
+        SearchEngine.searchField.keyup(function (event) {
             // Escape button
             if (event.which == 27) {
-                $('#ResultWrapper').fadeOut();
-
-                return;
+                // Hide result without clearing result list.
+                return SearchEngine._hideResults(false);
             }
 
             var fulltext = $.trim($(this).val());
 
             if (fulltext == "") {
-                return $('#ResultWrapper').html('').hide();
+                return SearchEngine._hideResults(true);
             }
 
+            // If user typed enough chars and search terms are different from the last search.
             if (fulltext.length > 3 && fulltext != SearchEngine.lastSearch) {
                 SearchEngine.lastSearch = fulltext;
                 SearchEngine._apiCall(fulltext);
             }
 
         });
+
+        // When search field loses focus.
+        SearchEngine.searchField.focusout(function() {
+            // Hide result without clearing result list.
+            SearchEngine._hideResults(false);
+        });
+
+        // Display last search's results when sibling search field.
+        SearchEngine.searchField.focusin(function() {
+           SearchEngine._showResults();
+        });
     },
 
     /**
-     * Make a call to API to get episodes matching given string.
+     * Make a call to the API to get episodes matching given search terms.
      *
      * @param {string} fulltext : the string to look for.
      * @private
@@ -44,6 +65,7 @@ var SearchEngine = {
             method: 'GET',
             contentType: 'application/json',
             timeout: 2000,
+            // url: Routing.generate('app.api.episodes.get', {fulltext: fulltext}),
             url: '/api/v1/episodes',
             data: {
                 fulltext: fulltext,
@@ -63,31 +85,159 @@ var SearchEngine = {
      * @private
      */
     _displayResult: function (results) {
-        var content = '';
-
+        var content = $('<ul>', {id: 'ResultList'});
 
         for (var i = 0; i < results.length; i++) {
+            // Episode item.
             var episode = results[i];
-            content += "<li class='ResultItem' data-nb='" + episode.number + "'><span>" + episode.title + "</span>";
 
-            if (episode.category != "") {
-                content += "<span class='details'>" + episode.category.name + "</span>";
+            var item = $('<li>', {
+                'class': 'ResultItem',
+                'data-nb': episode.number,
+            }).append($('<span>', {
+                class: 'ResultItem-title',
+                html: SearchEngine._highlightString(episode.title),
+            }));
+
+            // Episode's category.
+            if (typeof episode.category != 'undefined') {
+                item.append($('<span>', {
+                    class: 'ResultItem-category',
+                    html: SearchEngine._highlightString(episode.category.name),
+                }));
             }
 
+            // Episode's keywords.
             if (episode.keywords != "" && episode.keywords != null) {
-                content += '<span class="keywords">'+ episode.keywords +'</span>';
+                var highlight = SearchEngine._highlightKeywords(episode.keywords);
+
+                item.append($('<span>', {
+                    class: 'ResultItem-keywords',
+                    html: highlight,
+                }));
             }
 
-            content += "</li>";
+            // Event listener when click on a result item.
+            item.click(function () {
+                var nb = $(this).attr('data-nb');
+
+                Player.playEpisodeNb(nb);
+
+                $('#ResultWrapper').removeClass('opened');
+            });
+
+            content.append(item);
         }
 
-        $('#ResultWrapper').html(content).show();
+        if (!results.length) {
+            content.append($('<li>', {
+                html: 'Aucun résultat pour <i>' + SearchEngine.lastSearch + '</i>',
+            }));
+        }
 
-        // When click on an episode in the result list.
-        $('.ResultItem').click(function() {
-            var nb = $(this).attr('data-nb');
+        $('#ResultWrapper').html(content).addClass('opened');
+    },
 
-            Player.playEpisodeNb(nb);
-        });
+    /**
+     * Highlight search terms in the given string.
+     * None case sensitive.
+     *
+     * @param {string} string
+     * @returns {string}
+     * @private
+     */
+    _highlightString: function(string) {
+        var highlight = '';
+
+        var index = string.toLowerCase().indexOf(SearchEngine.lastSearch.toLowerCase());
+
+        if (index != -1) {
+            // Get possible chars before search terms.
+            highlight += string.substr(0, index);
+
+            // Highlight search terms.
+            highlight += '<span class="highlight">' + string.substr(index, SearchEngine.lastSearch.length) + '</span>';
+
+            // Get possible chars after search terms.
+            highlight += string.substr(index + SearchEngine.lastSearch.length);
+        }
+
+        if (highlight == '') {
+            return string;
+        }
+
+        return highlight;
+    },
+
+    /**
+     * Highlight search terms in the given string and truncate result string adding ellipsis before and after.
+     * None case sensitive.
+     *
+     * @param {string} keywords : the full string containing all keywords of an episode.
+     * @returns {string}
+     * @private
+     */
+    _highlightKeywords: function (keywords) {
+        // Total length of the sentence.
+        var totalLength = 50;
+
+        // Nb of chars before the fulltext string.
+        var charsBefore = 10;
+
+        // Find index of the fulltext string in keywords.
+        var index = keywords.toLowerCase().indexOf(SearchEngine.lastSearch.toLowerCase());
+
+        var highlight = '';
+
+        // Take few chars before and after the fulltext string.
+        if (index != -1) {
+            var rest = totalLength - charsBefore - SearchEngine.lastSearch.length;
+
+            if (rest < 0) {
+                rest = 0;
+            }
+
+            // Prepend ellipsis.
+            highlight += '...';
+
+            // Get few chars before the fulltext string.
+            highlight += keywords.substr(index - charsBefore, charsBefore);
+
+            // Highlight the fulltext string.
+            highlight += '<span class="highlight">' + keywords.substr(index, SearchEngine.lastSearch.length) + '</span>';
+
+            // Get few chars after the fulltext string.
+            highlight += keywords.substr(index + SearchEngine.lastSearch.length, rest);
+
+            // Append ellipsis.
+            highlight += '...';
+        }
+
+        return highlight;
+    },
+
+    /**
+     * Hide result wrapper.
+     *
+     * @param {boolean} clearContent : clear results if TRUE (optional).
+     * @private
+     */
+    _hideResults: function (clearContent) {
+        var wrapper = $('#ResultWrapper');
+
+        wrapper.removeClass('opened');
+
+        if (typeof clearContent == 'boolean' && clearContent == true) {
+            wrapper.html('');
+        }
+    },
+
+    /**
+     * Show result wrapper.
+     *
+     * @private
+     */
+    _showResults: function () {
+        $('#ResultWrapper').addClass('opened');
     }
 };
