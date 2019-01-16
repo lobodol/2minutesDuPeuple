c5baebebf8246773f2b21fa51fb1acba379cbd1e
lobodol <grobodol@gmail.com>
2019-01-16 10:41:30 +0100

    Add meta description on homepage

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, user-scalable=no">
+    <meta name="description" content="Le player du peuple, le meilleur moyen d'écouter et de rechercher un épisode des 2 minutes du peuple de François Pérusse !">
 
     <title>Les 2 minutes du peuple</title>
 
